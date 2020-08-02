567a9e9f29f6b24bd4365e3e8c7b3f5ed94fba48
ALEX\alex <lyssenko.alex@gmail.com>
2020-08-02 14:45:44 +0600

    feat: logo

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,13 +1,33 @@
-import React from 'react';
-
+import React from "react";
 
 const Aside = () => {
+  return (
+    <aside>
+      <h1>Scheduler</h1>
+      <div className="scheduler_container">
+        <div className="title">
+          <span>1h, 2 times a week</span>
+        </div>
+        <div>
+          <span>Monday:</span>
+        </div>
+        <div>
+          <span>7:45 AM / 6:15 PM</span>
+        </div>
+        <div>
+          <span>Wednesday:</span>
+        </div>
+        <div>
+          <span>7:45 AM / 6:15 PM</span>
+        </div>
+        <div className="title">
+          <span>3000kzt for 1h</span>
+        </div>
+      </div>
 
-    return (
-        <aside>
-            <h1>homework is must</h1>
-        </aside>
-    )
-}
+      <h1>homework is must</h1>
+    </aside>
+  );
+};
 
 export default Aside;
