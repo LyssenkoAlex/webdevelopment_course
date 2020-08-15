c554e9fa126aa1f1efd4cfdd581f5ad9e170e36a
ALEX\alex <lyssenko.alex@gmail.com>
2020-08-15 17:28:14 +0600

    fix: title

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
 
     return (
             <nav>
-                <h1>Lear to make HTML pages</h1>
+                <h1>Learn how to make HTML pages</h1>
                 <h1>Start to build your portfolio</h1>
             </nav>
     )
